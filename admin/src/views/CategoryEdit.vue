753d57789ed1e3bf170a838f8a0e8c7920a6757c
giriawsh <giriawsh@outlook.com>
2021-03-18 21:16:02 +0800

    封装通用增删改查接口

--- admin/src/views/CategoryEdit.vue
+++ admin/src/views/CategoryEdit.vue
@@ -41,9 +41,9 @@ export default {
     async save() {
       let res;
       if (this.id) {
-        res = await this.$http.put(`categories/${this.id}`, this.model);
+        res = await this.$http.put(`rest/categories/${this.id}`, this.model);
       } else {
-        res = await this.$http.post('categories', this.model);
+        res = await this.$http.post('rest/categories', this.model);
       }
       console.log(res);
       this.$router.push('/categories/list');
@@ -54,11 +54,11 @@ export default {
 
     },
     async fetch() {
-      const res = await this.$http.get(`categories/${this.id}`);
+      const res = await this.$http.get(`rest/categories/${this.id}`);
       this.model = res.data;
     },
     async fetchParents() {
-      const res = await this.$http.get(`categories`);
+      const res = await this.$http.get(`rest/categories`);
       this.parents = res.data;
     }
   },
--- admin/src/views/CategoryList.vue
+++ admin/src/views/CategoryList.vue
@@ -30,7 +30,7 @@ export default {
   },
   methods: {
     async fetch() {
-      const res = await this.$http.get('categories')
+      const res = await this.$http.get('rest/categories')
       this.items = res.data;
     },
     async remove(row) {
@@ -39,7 +39,7 @@ export default {
         cancelButtonText: '取消',
         type: 'warning'
       }).then(async () => {
-        await this.$http.delete(`categories/${row._id}`)
+        await this.$http.delete(`rest/categories/${row._id}`)
         this.$message({
           type: 'success',
           message: '删除成功!'
