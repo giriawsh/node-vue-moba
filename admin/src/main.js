9ddec881234f6d91a3dc6e84df30143ba63d8bb5
giriawsh <giriawsh@outlook.com>
2021-03-20 17:30:07 +0800

    添加工具样式

--- admin/src/main.js
+++ admin/src/main.js
@@ -5,8 +5,25 @@ import router from './router'
 import http from "@/http";
 
 import './style.css'
+
 const app = createApp(App).use(router)
 app.config.globalProperties.$http = http;
+
+
 // app.use(http(app));
 installElementPlus(app)
+app.mixin({
+    computed:{
+        uploadUrl() {
+            return this.$http.defaults.baseURL + '/upload'
+        }
+    },
+    methods: {
+        getAuthHeaders() {
+            return {
+                Authorization: `Bearer ${localStorage.token}`
+            }
+        }
+    }
+})
 app.mount('#app')
