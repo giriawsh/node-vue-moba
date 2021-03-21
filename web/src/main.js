31061bdd0ced1e519d02eb51f7256b946d174328
giriawsh <giriawsh@outlook.com>
2021-03-21 16:35:05 +0800

    添加公共样式

--- web/src/main.js
+++ web/src/main.js
@@ -2,4 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import './style.scss';
-createApp(App).mount('#app')
+import router from './router'
+createApp(App).use(router).mount('#app')
